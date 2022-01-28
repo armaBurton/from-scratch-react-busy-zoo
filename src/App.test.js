8c88daf4b2d7695f401ca471dd39c20fd819c018
armaBurton <armanarma@armaburton.com>
2022-01-28 12:45:44 -0800

    updated app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('SpAgHeTtI mOnStAr', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/SpAgHeTtI mOnStAr/i);
   expect(linkElement).toBeInTheDocument();
 });
