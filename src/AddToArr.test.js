f002cad9e7698b963fb19f18ec42c391a2bda105
armaBurton <armanarma@armaburton.com>
2022-02-17 15:07:27 -0800

    added another test

---
+++ src/AddToArr.test.js
@@ -0,0 +1,13 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from './App';
+
+test('should add a color to the array when monster button is clicked', async () => {
+  render(<App />);
+  
+  const greenMonsterButton = await screen.findByText(`green monster`);
+  fireEvent.click(greenMonsterButton);
+  
+  const greenMonster = await screen.findByAltText(`green monster`);
+  expect(greenMonster.alt).toBe('green monster');
+
+});
